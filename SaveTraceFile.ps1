bcfa35c7e2dda143c63b2721d4ea27059fe15318
Su, Jun-Ming <sujunmin@gmail.com>
2016-09-26 16:21:41 +0800

    Modify that there will be only data which was specified into database.

--- SaveTraceFile.ps1
+++ SaveTraceFile.ps1
@@ -1,24 +1,13 @@
-$AuditDBServer = "."
-$AuditDB = "AuditDB"
-$TraceFileData = "TraceFileData"
-$AuditDBUserName = "username"
-$AuditDBPassword = "password"
 $SaveFileDir = "D:\ArchivedTraceFiles"
-$BatchSize = 10000
 $7zipPath = "C:\Program Files\7-Zip\7z.exe"
 
-$SaveFileDate = (Get-Date).AddDays(-2)
+$SaveFileDate = (Get-Date).AddDays(-1)
 
 $DirYear =  "{0:yyyy}" -f $SaveFileDate
 $DirMonth = "{0:MM}" -f $SaveFileDate
 $SaveFileName = "{0:yyyy-MM-dd}" -f $SaveFileDate
 
-try{
-
-	$sql = "select * from " + $AuditDB + ".dbo." + $TraceFileData + " where StartTime < '" + ("{0:yyyy-MM-dd}" -f $SaveFileDate.AddDays(1)) + "'"
-
-	& bcp "$($sql)" queryout "$SaveFileDir\$SaveFileName.csv" -c -t"\t" -r"|" -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword
-
+try{	
 	If( -Not (Test-Path "$SaveFileDir\$DirYear")) {
 		New-Item -Path "$SaveFileDir" -name "$DirYear" -type Directory
 	}
@@ -29,13 +18,10 @@ try{
 
 	If( -Not (Test-Path "$SaveFileDir\$DirYear\$DirMonth\$SaveFileName.zip")) {
 
-		&$7zipPath a -tzip "$SaveFileDir\$DirYear\$DirMonth\$SaveFileName.zip" "$SaveFileDir\$SaveFileName.csv"
+		&$7zipPath a -tzip "$SaveFileDir\$DirYear\$DirMonth\$SaveFileName.zip" "$SaveFileDir\$SaveFileName\*"
 
-		$sql = "select 'starting'; while @@ROWCOUNT <> 0 delete TOP (" + $BatchSize + ") from " + $AuditDB + ".dbo." + $TraceFileData + " where StartTime < '" + ("{0:yyyy-MM-dd}" -f $SaveFileDate.AddDays(1)) + "'"
-
-		& sqlcmd -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword -Q "$($sql)" -d $AuditDB
-
-		Remove-Item "$SaveFileDir\$DirYear\$DirMonth\$SaveFileName.csv"
+		Remove-Item -Recurse -Force "$SaveFileDir\$SaveFileName"
 	}
 } catch {}	
+
 
--- TraceFileParser.ps1
+++ TraceFileParser.ps1
@@ -4,9 +4,10 @@ $TraceFileData = "TraceFileData" # Table name that saves trace data
 $AuditDBUserName = "username" # Can access AuditDB username, and need to have permission for alter trace on server level
 $AuditDBPassword = "password"
 $FromDirs = "E:\dir1","E:\dir2","E:\dir3" # Collect trace files from $FromDirs
-$ErrorDir = "E:\errdir\" # If errors then save original trace files into $ErrorDir
+$SaveFileDir = "D:\ArchivedTraceFiles" # Trace files save to
 $MailColumns = "StartTime, LoginName, HostName, ServerName, DatabaseName, ApplicationName, TextData, EventClass" #Set columns for alerts
 $MailEventClasses = "102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 116, 117, 118, 128, 129, 130, 131, 132, 133, 134, 135, 136, 152, 153, 170, 171, 172, 173, 174, 175, 176, 177" # Set EventClass for alerts
+$Users = "'username'" # Need to save into database
 $MailFrom = "from@abc.com" # Mail from
 $MailTo = "to@def.com" # Rcpt To
 $MailServer = "smtp.server" # Mail server
@@ -17,23 +18,27 @@ $FromDirs | Foreach-Object {
 		try { 
 			[IO.File]::OpenWrite($_.FullName).close() # Test file can Be processed
 
-			$sql = "insert into " + $TraceFileData + " select *  FROM fn_trace_gettable(N'" + $_.FullName + "', 1)"
-	    		$output = & sqlcmd -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword -Q "$($sql)" -d $AuditDB # Insert trace file data into table
- 
-			if ($output -like "*個受影響的資料列*") { # Affected Rows in Chinese
-				$sql = "execute savetableashtml @DBFetch='select " + $MailColumns + " FROM fn_trace_gettable(N^" + $_.FullName + "^, 1)', @DBWhere='EventClass in (" + $MailEventClasses + ")', @DBThere='StartTime'"
-				$output = & sqlcmd -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword -Q "$($sql)" -d $AuditDB # Do some alerts for specified eventclasses				
+			If( -Not (Test-Path ("$SaveFileDir\" + ('{0:yyyy-MM-dd}' -f (get-childitem $_.FullName).creationtime)))) {
+				New-Item -Path "$SaveFileDir" -name "$('{0:yyyy-MM-dd}' -f (get-childitem $_.FullName).creationtime)" -type Directory
+			}
 
-				if ($output -like "*<table*") { # Have data to output
-					Send-MailMessage -To $MailTo -From $MailFrom -Subject "資料庫特權活動即時告警" -Body "$output" -BodyAsHtml -SmtpServer $MailServer -Encoding ([System.Text.Encoding]::UTF8)
-				}
+			$sql = " select * FROM fn_trace_gettable(N'" + $_.FullName + "', 1)"
+			$outfile = "$SaveFileDir\" + ('{0:yyyy-MM-dd}' -f (get-childitem $_.FullName).creationtime) + "\" + (get-date -UFormat %s) + ".csv"
 
-        			Remove-Item $_.FullName
-    			} else {
-				$rand = Get-Random
-				$newfilename =  $ErrorDir + [string]$rand + ".trc"
-				Move-Item -Path $_.FullName -Destination $newfilename
-    			}
+			& bcp "$($sql)" queryout "$outfile" -c -t"\t" -r"|" -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword
+
+			$sql = "insert into " + $TraceFileData + " select *  FROM fn_trace_gettable(N'" + $_.FullName + "', 1) where LoginName in (" + $Users + ")" # Only $Users activities will be into database
+		    	$output = & sqlcmd -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword -Q "$($sql)" -d $AuditDB 		
+
+
+			$sql = "execute savetableashtml @DBFetch='select " + $MailColumns + " FROM fn_trace_gettable(N^" + $_.FullName + "^, 1)', @DBWhere='EventClass in (" + $MailEventClasses + ")', @DBThere='StartTime'"
+			$output = & sqlcmd -S $AuditDBServer -U $AuditDBUserName -P $AuditDBPassword -Q "$($sql)" -d $AuditDB				
+				
+			if ($output -like "*<table*") {
+				Send-MailMessage -To $MailTo -From $MailFrom -Subject "資料庫特權活動即時告警" -Body "$output" -BodyAsHtml -SmtpServer $MailServer -Encoding ([System.Text.Encoding]::UTF8)		
+			} 
+
+        		Remove-Item $_.FullName
 		}
 		catch {}
 
